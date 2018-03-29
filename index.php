d8cd790e888f899fe41cd637fb2c0b61d0b0dd1a
n7rc <notrec@hotmail.com>
2018-03-29 11:00:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once ("config.inc.php");
+require_once ("functions.inc.php");
+
 $date   = date('Y-m-d');
 $time   = date('H:i');
 $tomorrow = strtotime ('+1 day', strtotime ($date)) ;
