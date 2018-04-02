5f62e3f445c5fdff8df0b65cb039f4188b7632a4
n7rc <notrec@hotmail.com>
2018-04-02 12:02:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,69 +12,19 @@ $connection=mysqli_connect($mysqlhost, $mysqluser, $mysqlpwd, $mysqldb);
 mysqli_select_db($connection, $mysqldb);
 
 echo '<!DOCTYPE html>
-			<html>
-			<head>
-			<meta charset="UTF-8">
-			<link rel="stylesheet" type="text/css" href="css/style.css" />
-			<script type="text/javascript" src="jquery/jquery-1.11.3.js"></script>
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<!--<meta http-equiv="refresh" content="600" />-->
-			<title>' . $titlePage . '</title>';
-?>
-			<script type="text/javascript">
-			/*alterna el contenido de #booking-table (#today, #tomorrow)*/
-			//sin refresco de informaicón
-			function showToday(){
-				$('#hide').hide();
-				$('#today').show();
-			}
-			function showTomorrow(){
-				$('#today').hide();
-				$('#hide').show();
-			}
-
-			var flag=false;
-			setInterval(function(){
-  				if(flag){
-    					showToday();
-    					flag=false;
-  				}else{
-    					showTomorrow();
-    					flag=true;
-  				}
-			}, 60000);
-			//con refresco de informaicón
-			/*function loadToday(){
-				$('#booking-table').load('index.php #today');
-			}
-			function loadTomorrow(){
-				$('#booking-table').load('index.php #tomorrow');
-			}
-			//método 1.
-			var flag=false;
-			setInterval(function(){
-  				if(flag){
-    					loadToday();
-    					flag=false;
-  				}else{
-    					loadTomorrow();
-    					flag=true;
-  				}
-			}, 60000);*/
-			//método 2.
-			/*function delaytomorrow(){
-				setTimeout("loadtomorrow()", 60000);
-			}
-			setTimeout("loadtomorrow()", 60000);
-			setInterval("loadtoday()", 120000);
-			setInterval("delaytomorroww()", 120000);*/
-			</script>
-<?php
-
-echo '</head>
-			<body>';
-echo '<div id="booking-table" class="clear">
-			<div id="today">';
+      <html>
+      <head>
+      <meta charset="UTF-8">
+      <link rel="stylesheet" type="text/css" href="css/style.css" />
+      <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+      <script type="text/javascript" src="js/alternate.js"></script>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!--<meta http-equiv="refresh" content="600" />-->
+      <title>' . $titlePage . '</title>
+      </head>';
+echo '<body>
+      <div id="booking-table" class="clear">
+      <div id="today">';
 
 // Sala izquierda
 echo '<div class="room"><h3>' . getRoom($idroom_left);
@@ -111,8 +61,8 @@ if (occupationState(qryState($date, $idroom_right)) == 1){
 				<a class="signup">Libre</a>';
 }
 echo '</div></div>
-			<div id="hide" style="display:none;">
-			<div id="tomorrow">';
+      <div id="hide" style="display:none;">
+      <div id="tomorrow">';
 
 // Sala izquierda
 echo '<div class="room"><h3>' . getRoom($idroom_left);
@@ -149,6 +99,6 @@ if (occupationState(qryState($tomorrow, $idroom_right)) == 1){
 				<a class="signup">Libre</a>';
 }
 echo '</div>
-			</div></div>
-			</div></body></html>';
+      </div></div>
+      </div></body></html>';
 ?>
